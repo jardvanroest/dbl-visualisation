8bb990cdc3ab0931d1b6c92134e5385f6ea8baa3
Velyan Kolev <kolev.velyan@gmail.com>
2021-05-15 13:53:57 +0300

    added components for each field and calculate average sentiment and dates (first/last)

--- src/components/Inspector.vue
+++ src/components/Inspector.vue
@@ -3,21 +3,23 @@
     <div v-if="emailsExist">
       <div class="section">Sender</div>
       <hr />
-      <div>From: {{ email["fromEmail"].slice(0, -10) }}</div>
-      <div>ID: {{ email["fromId"] }}</div>
-      <div>Job title: {{ email["fromJobtitle"] }}</div>
+      <inspectorField field="Email" :info="email['fromEmail']" />
+      <inspectorField field="Id" :info="email['fromId']" />
+      <inspectorField field="Job" :info="email['fromJobtitle']" />
       <div class="section">Recipient</div>
       <hr />
-      <div>To: {{ email["toEmail"].slice(0, -10) }}</div>
-      <div>ID: {{ email["toId"] }}</div>
-      <div>Job title: {{ email["toJobtitle"] }}</div>
+      <inspectorField field="Email" :info="email['toEmail']" />
+      <inspectorField field="Id" :info="email['toId']" />
+      <inspectorField field="Job" :info="email['toJobtitle']" />
       <div class="section">Additional information</div>
       <hr />
-      <div>Date: {{ email["date"] }}</div>
-      <div>Emials sent: {{ newData["weight"] }}</div>
-      <div>Average sentiment: {{ averageSentiment }}</div>
+      <inspectorField field="Emails" :info="newData['weight']" />
+      <inspectorField v-if="!sameDate" field="First date" :info="minDate" />
+      <inspectorField v-if="!sameDate" field="Last date" :info="maxDate" />
+      <inspectorField v-if="sameDate" field="Date" :info="minDate" />
+      <inspectorField field="Average sentiment" :info="avgSentiment" />
       <div class="nodeColor">
-        <div>Node color: {{ nodeColor.toLowerCase() }}</div>
+        <inspectorField field="Node color" :info="nodeColor" />
         <svg>
           <rect
             v-bind:style="{ fill: nodeColor }"
@@ -29,21 +31,22 @@
         </svg>
       </div>
     </div>
-    <div v-else>No emails have been sent between these two people!</div>
+    <div v-else>Click on a node to dislay information about it!</div>
   </div>
 </template>
 
 <script>
+import InspectorField from "@/components/InspectorField.vue";
 import { mapGetters } from "vuex";
 
 export default {
   name: "Inspector",
+  components: {
+    InspectorField,
+  },
   computed: {
     ...mapGetters(["getInspectorData"]),
   },
-  // created() {
-  //   this.incomingNewData(this.displayedData);
-  // },
   watch: {
     // Watch for a new incoming {inspectorData}
     getInspectorData(newData) {
@@ -56,20 +59,56 @@ export default {
       title: "Email correspondance",
       newData: "none",
       email: "none",
-      averageSentiment: 0,
       nodeColor: "#FFFFFF",
+      dates: [],
+      minDate: 0,
+      maxDate: 0,
+      sameDate: false,
+      avgSentiment: 0,
+      sentiments: [],
+      messageTypes: [],
     };
   },
   methods: {
     incomingNewData(newData) {
       const dataset = this.$store.state.dataset.getRawData();
-      console.clear();
-      console.log(newData);
 
       this.emailsExist = newData["weight"] > 0;
       this.email = dataset[newData["dataIndex"][0]];
       this.newData = newData;
-      this.nodeColor = newData["fillColor"];
+      this.nodeColor = newData["fillColor"].toLowerCase();
+
+      // If the two people have sent emails between eachother
+      if (this.emailsExist) {
+        // Reset dates
+        this.dates = [];
+        this.maxDate = new Date("1001-01-01");
+        this.minDate = new Date("3001-01-01");
+
+        newData["dataIndex"].forEach((index) => {
+          // Get all sentiments and messageTypes
+          this.sentiments.push(parseFloat(dataset[index]["sentiment"]));
+          this.messageTypes.push(dataset[index]["messageType"]);
+
+          // Get all dates and calculate the first and last one
+          var currentDate = new Date(dataset[index]["date"]);
+          this.dates.push(currentDate.toDateString());
+
+          if (currentDate < this.minDate) this.minDate = currentDate;
+          if (currentDate > this.maxDate) this.maxDate = currentDate;
+        });
+        // Check if same date
+        if (this.minDate === this.maxDate) this.sameDate = true;
+        else this.sameDate = false;
+
+        // Convert dates to correct format
+        this.minDate = this.minDate.toDateString();
+        this.maxDate = this.maxDate.toDateString();
+
+        // Get average sentiment
+        const arrAvg = (arr) => arr.reduce((a, b) => a + b, 0) / arr.length;
+        this.avgSentiment = arrAvg(this.sentiments).toPrecision(3);
+      }
     },
   },
 };
@@ -81,7 +120,7 @@ export default {
   padding-bottom: 25px;
   font-size: 10pt;
 
-  overflow-y: scroll;
+  overflow-y: auto;
   overflow-x: hidden;
 }
 
@@ -104,6 +143,7 @@ export default {
 /* Style horizontal lines */
 hr {
   margin-top: 3px;
+  margin-bottom: 1px;
   margin-right: 5px;
   border: none;
   border-top: 1px solid var(--border-color);
@@ -115,6 +155,7 @@ hr {
 
   font-weight: bold;
   font-size: 12pt;
+  text-transform: capitalize;
   color: var(--accent-color);
 }
 </style>
---
+++ src/components/InspectorField.vue
@@ -0,0 +1,19 @@
+<template>
+  <span class="field">{{ field }}: </span>
+  <span>{{ info }}</span>
+  <br />
+</template>
+
+<script>
+export default {
+  name: "InspectorField",
+  props: ["field", "info"],
+};
+</script>
+
+<style scoped>
+.field {
+  color: var(--accent-color);
+  margin-left: 0.7em;
+}
+</style>
