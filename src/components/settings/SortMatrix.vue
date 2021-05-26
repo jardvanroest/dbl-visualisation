24168ad79b064b350cafc5da71fecb15016f6774
Velyan Kolev <kolev.velyan@gmail.com>
2021-05-26 17:31:10 +0300

    removed unused code

--- src/components/settings/SortMatrix.vue
+++ src/components/settings/SortMatrix.vue
@@ -25,13 +25,7 @@ export default {
     ...mapActions("dataset", ["changeSortedMatrixData"]),
     sortMatrix() {
       let data = this.getMatrixData;
-
-      // NOT REALLY WORKING RIGHT NOW
-      // CuthillMckee algorithm made by (c) 2015 Mikola Lysenko. MIT
-      // https://github.com/mikolalysenko/cuthill-mckee
       // let coeffs = this.getCoefficients();
-      // let perm = this.cuthillMckee(coeffs, coeffs.length);
-      // let permutedData = this.permuteMatrix(data, perm, perm);
 
       // Do the selected sorting algorithm
       switch (this.sortAlg) {
@@ -80,60 +74,6 @@ export default {
         (prev, next) => next.map((item, i) => (prev[i] || []).concat(next[i])),
         []
       );
-    },
-    swapArrayElements(arr, el1, el2) {
-      let temp = arr[el1];
-      arr[el1] = arr[el2];
-      arr[el2] = temp;
-
-      return arr;
-    },
-    compareNum(a, b) {
-      return a - b;
-    },
-    cuthillMckee(list, n) {
-      var adj = new Array(n);
-      var visited = new Array(n);
-      for (let i = 0; i < n; ++i) {
-        adj[i] = [];
-        visited[i] = false;
-      }
-
-      for (let i = 0; i < list.length; ++i) {
-        var l = list[i];
-        adj[l[0]].push(l[1]);
-      }
-
-      var toVisit = new Array(n);
-      var eol = 0;
-      var ptr = 0;
-      for (let i = 0; i < n; ++i) {
-        if (visited[i]) {
-          continue;
-        }
-        toVisit[eol++] = i;
-        visited[i] = true;
-        while (ptr < eol) {
-          var v = toVisit[ptr++];
-          var nbhd = adj[v];
-          nbhd.sort(this.compareNum);
-          for (var j = 0; j < nbhd.length; ++j) {
-            var u = nbhd[j];
-            if (visited[u]) {
-              continue;
-            }
-            visited[u] = true;
-            toVisit[eol++] = u;
-          }
-        }
-      }
-
-      var result = new Array(n);
-      for (let i = 0; i < n; ++i) {
-        result[toVisit[i]] = i;
-      }
-
-      return result;
     },
   },
 };
