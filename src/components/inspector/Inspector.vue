24125a81dc5451ca6be22b673bb6db56eb53f870
Velyan Kolev <kolev.velyan@gmail.com>
2021-06-09 19:13:24 +0300

    implemented inspector in calendar matrix

--- src/components/inspector/Inspector.vue
+++ src/components/inspector/Inspector.vue
@@ -115,7 +115,11 @@ export default {
     titleCase(str) {
       var splitStr = str.toLowerCase().split(" ");
       for (var i = 0; i < splitStr.length; i++) {
-        if (splitStr[i] !== "and") {
+        if (
+          splitStr[i] !== "and" &&
+          splitStr[i] !== "in" &&
+          splitStr[i] !== "of"
+        ) {
           splitStr[i] =
             splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);
         }
