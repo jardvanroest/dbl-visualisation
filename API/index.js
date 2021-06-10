de64065313157b4913a1e4581bd61ea1dba89c16
Macovei Tudor <tudor_macovei@iCloud.com>
2021-06-10 09:41:14 +0200

    51-Host Website (#36)
    * Initial commit API
    
    Adds a barebones server with functionality for:
    -JSON file upload via http PUT requests
    -JSON file download via http GET requests, for single and multiple JSON objects
    
    * Delete unused file
    
    * Add reading & writing to disk
    
    Also small changes to some vue components that will be used for the dataset upload feature
    
    * Fix error for GET request when datasets.json does not exist
    
    * Add dataset upload when importing non-default dataset
    
    * Fix unused CORS policy
    
    * Add basic functionality of getting a dataset, and rerouting to the visualization page
    
    * Add animated loading screen, remove button
    
    * Delete unnecessary comments
    
    * Add popup component and datasetID to the store
    
    * Finalize basic functionality
    
    * Handle link to default dataset
    
    * Add error handling for incorrect /get links
    
    * Beautify the share link pop-up
    
    * fixed styling + click action
    
    * made button bigger
    
    * Minor bugfixes after resolving conflicts with main
    
    * loading page styling
    
    * Add requested changes
    
    * Add server IP environment variable
    
    It changes based on the build setting
    
    * New production IP
    
    * Add HOST script to use when hosting publicly
    
    * Update package-lock.json
    
    Co-authored-by: Velyan Kolev <kolev.velyan@gmail.com>

--- API/index.js
+++ API/index.js
@@ -18,5 +18,5 @@ app.get("/", (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running on port: http://localhost:${PORT}`);
+  console.log(`Server running on port: ${PORT}`);
 });
