00adf2e1b2a8c2b260afd8ccf48ff268cf2c6426
jardvanroest <35775988+jardvanroest@users.noreply.github.com>
2021-06-11 20:16:05 +0200

    48 filter jobtitle (#39)
    * Fix bug that caused container div to not grow
    
    * Create UI elements
    
    * Add a getter to get jobtitles from store
    
    * Add jobtitles dynamically from dataset
    
    * Run npm install
    
    * Make job title filter work
    
    * Create one single button to apply all filters
    
    * Remove unnecessary code
    
    * fixed sizing of settings and header
    
    * scrollbar styling
    
    * styling of jobtitle filter and fixing inspector to show correctly {filtered} property
    
    * Reset old dataset correctly before saving new one
    
    Co-authored-by: Velyan Kolev <kolev.velyan@gmail.com>

--- src/components/settings/Setting.vue
+++ src/components/settings/Setting.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="setting-cont" :class="{ flexRow: flexRow === 'yes' }">
     <p>{{ name }}:</p>
+    <hr />
     <slot></slot>
   </div>
 </template>
@@ -30,5 +31,15 @@ export default {
 
 .setting-cont p {
   font-size: 1rem;
+  margin-left: 0.5em;
+  margin-bottom: 0;
+}
+
+/* Style horizontal lines */
+hr {
+  width: 75%;
+  margin: 3px 2em 0.5em 0em;
+  border: none;
+  border-top: 1px solid var(--border-color);
 }
 </style>
