759e4eb5eb62d93c75bc2c91bb5ec1fe9e6a27f8
Velyan Kolev <kolev.velyan@gmail.com>
2021-06-12 12:45:44 +0300

    sortMatrix.vue now uses the DropDown component

--- src/components/settings/SortMatrix.vue
+++ src/components/settings/SortMatrix.vue
@@ -1,21 +1,18 @@
 <template>
-  <div class="sort-container">
-    <div class="sort-select">
-      <select id="sorting-selector" @change="sortMatrix">
-        <option value="initial">Initial Order</option>
-        <option value="random">Random Order</option>
-        <option value="optimal_leaf">Optimal Leaf Order</option>
-        <option value="pca">PCA Order</option>
-        <option value="spectral">Spectral Order</option>
-      </select>
-      <span class="custom-arrow" />
-    </div>
+  <div class="sort-matrix-container">
+    <DropDown
+      class="dropdown"
+      selected="initial"
+      :items="dropdownItems"
+      @changed="sortMatrix"
+    />
   </div>
 </template>
 
 <script>
 import * as d3 from "d3";
 import * as reorder from "reorder.js";
+import DropDown from "@/components/DropDown.vue";
 import { mapGetters, mapActions } from "vuex";
 
 export default {
@@ -23,24 +20,24 @@ export default {
   computed: {
     ...mapGetters("dataset", ["persons", "getMatrixDataForSorting"]),
   },
+  components: {
+    DropDown,
+  },
   data() {
     return {
-      sortAlg: { type: "initial", index: 0 },
+      dropdownItems: [
+        { value: "initial", name: "Initial order" },
+        { value: "random", name: "Random order" },
+        { value: "optimal_leaf", name: "Optimal leaf order" },
+        { value: "pca", name: "PCA Order" },
+        { value: "spectral", name: "Spectral Order" },
+      ],
     };
-  },
-  mounted() {
-    // NOT WORKING make selected option stay the same even after settings being hidden
-    const select = document.getElementById("sorting-selector");
-    select.selectedIndex = this.sortAlg["index"];
   },
   methods: {
     ...mapActions("dataset", ["changeSortedMatrixData"]),
-    sortMatrix(event) {
-      // Do the selected sorting algorithm
-      this.sortAlg["type"] = event.target.value;
-      this.sortAlg["index"] = event.target.options.selectedIndex;
-
-      switch (this.sortAlg["type"]) {
+    sortMatrix(value) {
+      switch (value) {
         case "initial":
           this.changeSortedMatrixData({
             personsRows: this.persons,
@@ -60,7 +57,7 @@ export default {
           this.spectralSort();
           break;
         default:
-          console.log(this.sortAlg["type"]);
+          console.log(value);
       }
     },
     optimalLeafOrder() {
@@ -132,75 +129,7 @@ export default {
 </script>
 
 <style scoped lang="scss">
-.sort-container {
+.sort-matrix-container {
   display: flex;
-}
-
-.sort-select {
-  font: inherit;
-  position: relative;
-}
-
-#sorting-selector {
-  border-radius: 0.32em;
-  padding: 0.3em 1.5em 0.3em 0.3em;
-  background-color: var(--background-color);
-  border: var(--settings-border);
-
-  &:focus {
-    border: 2px solid black;
-  }
-  &:focus * {
-    color: var(--accent-color);
-  }
-  &:focus + .custom-arrow {
-    border: 2px solid black;
-    border-left: 0;
-  }
-}
-
-/* Make custom arrow for drop-down menu */
-.custom-arrow {
-  display: block;
-  position: absolute;
-
-  top: 0;
-  right: 0;
-  height: calc(100% - 4px);
-  width: 2em;
-
-  border: var(--settings-border);
-  border-left: 0;
-  border-top-right-radius: 0.32em;
-  border-bottom-right-radius: 0.32em;
-  background: var(--accent-color);
-  pointer-events: none;
-}
-
-.custom-arrow::before,
-.custom-arrow::after {
-  --size: 0.45em;
-
-  content: "";
-  position: absolute;
-  width: 0;
-  height: 0;
-
-  left: 50%;
-  top: 50%;
-  transform: translate(-50%, -50%);
-
-  border-left: var(--size) solid transparent;
-  border-right: var(--size) solid transparent;
-}
-
-.custom-arrow::before {
-  border-bottom: var(--size) solid var(--background-color);
-  top: 35%;
-}
-
-.custom-arrow::after {
-  border-top: var(--size) solid var(--background-color);
-  top: 65%;
 }
 </style>
